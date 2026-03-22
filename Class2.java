dbaedc226177a224255d66538c03f48ea998fdac
alisoncoder <aliceebare97@gmail.com>
2026-03-22 12:55:49 +0100

    Operators

---
+++ Class2.java
@@ -0,0 +1,18 @@
+class Class2 {
+    
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int diff = a - b;
+        int mult = a * b;
+        int div = a / b;
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + mult);
+        System.out.println("Division: " + div);
+    }
+}
