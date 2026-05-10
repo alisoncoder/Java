7406d9af0eacbdb89b2cae9f0951c66600d2af94
alisoncoder <aliceebare97@gmail.com>
2026-05-10 12:45:30 +0100

    interface

---
+++ Interface.java
@@ -0,0 +1,25 @@
+interface MyInterface
+{
+    void method1();
+    void method2();
+}
+
+public class Interface implements MyInterface
+{
+    public void method1()
+    {
+        System.out.println("implementation of method1");
+    }
+    public void method2()
+    {
+        System.out.println("implementation of method2");
+    }
+
+    public static void main(String[] args)
+    {
+        MyInterface obj = new Interface();
+
+        obj.method1();
+        obj.method2();
+    }
+}
