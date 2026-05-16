86df28d629ec7ffe226476b3ffa41a683573adf8
alisoncoder <aliceebare97@gmail.com>
2026-05-16 12:49:04 +0100

    exceptions

---
+++ Exception.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+ class Exception {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("enter two numbers:");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = x / y;
+            System.out.println(x + " / " + y + " = " + z);
+        } catch (ArithmeticException ex) {
+            System.out.println("--- catch block ---");
+            System.out.println(ex.toString());
+        } finally {
+            System.out.println("--- finally block ----");
+            System.out.println("Application Designed & Developed by");
+            System.out.println("nicole");
+            sc.close();
+        }
+        System.out.println("--- DONE ---");
+    }
+}
